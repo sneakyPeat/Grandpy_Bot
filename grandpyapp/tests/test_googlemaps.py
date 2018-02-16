95897b7eff5fdfea376ad315166d82b5df28a05c
sneakyPeat <david.ducluzeaud@live.fr>
2018-02-16 10:42:31 +0100

    use requests module

--- grandpyapp/tests/test_googlemaps.py
+++ grandpyapp/tests/test_googlemaps.py
@@ -1,21 +1,23 @@
 import json
-import urllib.request
+import request
+import os
 from io import BytesIO
 
 
 class TestGoogleMaps:
     def setup_metod(self):
-        self.query = "Salut Grandpy! Est ce que tu connais l'adresse d'OpenClassrooms?"
+        self.query = "OpenClassrooms"
         self.instance = GoogleMaps(self.query)
         assert isinstance(self.instance, GoogleMaps)
 
     def test_find_coordinates(self):
-        with open("gmaps_resulst_mock.json") as mock_file:
+
+        with open("mock_folder/gmaps_results_mock.json") as mock_file:
             results = json.load(mock_file)
 
         def mockreturn(request):
             return BytesIO(json.dumps(results).encode())
 
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
+        monkeypatch.setattr(request, 'get', mockreturn)
 
         assert self.instance.find_coordinates == results
