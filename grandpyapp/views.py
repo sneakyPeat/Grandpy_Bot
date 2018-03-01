fe9f317e81ad21051719abccebe071a65663ad92
sneakyPeat <david.ducluzeaud@live.fr>
2018-03-01 17:55:43 +0100

    add view for ajax interaction

--- grandpyapp/views.py
+++ grandpyapp/views.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from grandpyapp.query import Query
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('home_page.html')
+
+
+@app.route('/', methods=['POST'])
+def submit():
+    query = request.form['query']
+    response = Query(query)
+    response = response.collect_data()
+    return jsonify({"json": response})
