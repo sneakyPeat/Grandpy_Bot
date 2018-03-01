b6073373b2a63e8a9b3dd40ca984e686d6ad157b
sneakyPeat <david.ducluzeaud@live.fr>
2018-03-01 17:56:47 +0100

    ajax interactions

---
+++ grandpyapp/static/js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+
+        $.ajax({
+            url: '/',
+            type: 'POST',
+            dataType: 'json',
+            data: {query: $('#query').val()},
+            success:function(response){
+                console.log(response)
+                displayAjax(response);
+            }
+        })
+
+        event.preventDefault();
+
+    });
+});
+
+function displayAjax(response) {
+    var r_answer = random_answer()
+    var address = response.json.address
+    var latitude = response.json.lat
+    var longitude = response.json.lng
+    var title = response.json.title
+    var closest_thing_str = "La chose la plus proche que je connaisse est : "
+    var content = response.json.content
+    var url_content = response.json.wiki_link
+    // show the map
+    document.getElementById('map').style.display = 'block';
+
+    // display the adress
+    $('.grandpy_answer').html(r_answer + address)
+
+    // init the map
+    initMap(latitude, longitude);
+
+    // wikipedia info
+    $('.answer h4').html(closest_thing_str + title)
+    $('.wiki').html(content)
+    $('.wiki_link').attr('href', url_content).html("En savoir plus sur wikipedia ...")
+};
+
+function random_answer() {
+    var answer = ["Bien sûr mon poussin ! La voici : ",
+                  "Ah je me rappel très bien de cet endroit ",
+                  "A cet endroit, je me souviens : ",
+                  "Il y a bien longtemps que je ne suis pas allé ici : "
+                 ]
+
+    var random_number = Math.floor(Math.random() * answer.length);
+
+    return answer[random_number];
+};
+
+function initMap(latitude, longitude) {
+    var coord = {lat: latitude, lng: longitude};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+    return map
+};
