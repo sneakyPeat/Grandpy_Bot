df82859817658b9a56c6e392338f1c08962f8825
sneakyPeat <david.ducluzeaud@live.fr>
2018-03-05 14:57:57 +0100

    remove url

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ SECRET_KEY = "8smq3+zm823(xpj71v5-08zvo"
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 TOP_LEVEL_DIR = os.path.abspath(os.curdir)
 
-GM_GEOCODING_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 GM_GEOCODING_KEY = 'AIzaSyDKGJ_R9HNsdkeHiC3W1cIZ8e2e2vzRqAc'
