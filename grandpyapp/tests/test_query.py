181a8f1ee0f1ef0bec33088694a30fbc39dab95e
sneakyPeat <david.ducluzeaud@live.fr>
2018-03-12 11:22:34 +0100

    change test query

--- grandpyapp/tests/test_query.py
+++ grandpyapp/tests/test_query.py
@@ -5,18 +5,14 @@ import json
 
 class TestQuery:
     def setup_method(self):
-        """
-        Test if the object created is an instance of the query class
-        """
+        """Test if the object created is an instance of the query class."""
         self.query = "Salut Grandpy! Est ce que tu connais l'adresse d'OpenClassrooms?"
         self.instance = Query(self.query)
         assert isinstance(self.instance, Query)
 
     def test_parser_method(self):
-        """
-        Test the parse method in the Query class.
-        It must returned at least 1 element"""
-        assert len(self.instance.parse_question()) == 3
+        """Test the parse method in the Query class. It must returned at least 1 element."""
+        assert len(self.instance.parse_question()) == 1
 
     def test_collect_data(self):
         assert isinstance(self.instance.collect_data(), dict)
